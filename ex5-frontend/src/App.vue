49a71eedebc8c66c5296af103f38b902fd481ddd
pablo.barbosa <pablo.barbosa@anovasistemas.com.br>
2020-10-24 16:07:46 -0300

    inicializado front-end da aplicaçao

---
+++ ex5-frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div class="text-center">
+      <h1>Gerenciamento de Veículos</h1>
+      <span><i class="fas fa-car"></i></span>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css"
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
