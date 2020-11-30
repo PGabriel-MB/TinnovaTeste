5e987728a8afc7ce351fbe9470421b52ca778284
pablo.barbosa <pablo.barbosa@anovasistemas.com.br>
2020-11-30 10:05:08 -0300

    inserido icone no componente

--- ex5-frontend/src/components/CadastrarVeículo.vue
+++ ex5-frontend/src/components/CadastrarVeículo.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
-    <b-button v-b-modal.modal-1 variant='success'>Launch demo modal</b-button>
+    <b-button v-b-modal.modal-1 variant='success'>
+      Adicionar Veículo
+      <b-icon icon="plus-circle"></b-icon>
+    </b-button>
 
-    <b-modal id="modal-1" size="lg" title="BootstrapVue">
-      <p class="my-4">Hello from modal!</p>
+    <b-modal id="modal-1" size="lg" title="Cadastrar Veículo">
+      
     </b-modal>
   </div>
 </template>
 
 <script>
+import { BIcon } from "bootstrap-vue";
+
 export default {
   name: "CadastraVeiculo",
+  components:{
+    BIcon
+  },
   methods: {
       
   }
