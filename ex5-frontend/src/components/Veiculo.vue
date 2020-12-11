0c19c99350a2f514ba5d1ccc4b87f00a849b3eb5
pablo.barbosa <pablo.barbosa@anovasistemas.com.br>
2020-12-11 18:09:04 -0300

    finalizada a visualizaçao do veiculo e prestes a iniciar a exclusao de veiculos

--- ex5-frontend/src/components/Veiculo.vue
+++ ex5-frontend/src/components/Veiculo.vue
@@ -5,8 +5,51 @@
       <b-icon icon="eye"></b-icon>
     </b-button>
 
-		<b-modal id="modal-VisualizarVeiculo" size="lg" title="Visualizar Veículo">
-			{{ veiculo }}
+    <b-modal
+      id="modal-VisualizarVeiculo"
+      centered
+      size="lg"
+      title="Visualizar Veículo"
+    >
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <label for=""><strong>Veículo:</strong></label>
+            {{ veiculo.veiculo }}
+          </div>
+          <div class="col">
+            <label for=""><strong>Marca:</strong></label>
+            {{ veiculo.marca }}
+          </div>
+          <div class="col">
+            <label for=""><strong>Ano:</strong></label>
+            {{ veiculo.ano }}
+          </div>
+        </div>
+        <div class="row mt-3">
+          <div class="col">
+            <label for=""><strong>Descrição:</strong></label>
+            {{ veiculo.descricao }}
+          </div>
+          <div class="col">
+            <label for=""><strong>Vendido? </strong></label>
+            <span id="vendidoBadge" class="text-white">
+              <b-badge variant="success"> </b-badge>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <template #modal-footer>
+        <div>
+          <b-button variant="success">
+            <b-icon icon="check-circle"></b-icon>
+          </b-button>
+          <b-button variant="danger" class="ml-2">
+            <b-icon icon="x-circle"></b-icon>
+          </b-button>
+        </div>
+      </template>
     </b-modal>
   </div>
 </template>
@@ -14,9 +57,17 @@
 <script>
 export default {
   name: "Veiculo", // Visualizando detalhes do veiculo
-	props: ["veiculo"],
-	methods: {
-
-	}
+  props: ["veiculo"],
+  methods: {},
 };
 </script>
+
+<style>
+#vendidoBadge {
+  font-size: 2rem;
+}
+
+#modal-VisualizarVeiculo {
+  font-size: 1.5rem;
+}
+</style>
