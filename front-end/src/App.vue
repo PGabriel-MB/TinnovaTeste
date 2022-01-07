ad55a9e222594e8df054a8315fd5fb07b206544a
PGabriel-MB <pablogrbarbosa@gmail.com>
2022-01-06 21:59:31 -0300

    configurando rota inicial da aplicaçao

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,20 +3,13 @@
     <div class="text-center">
       <h1>Gerenciamento de Veículos <span><i class="fas fa-car"></i></span></h1>
     </div>
-    <Principal></Principal>
+    <router-view />
   </div>
 </template>
 
 <script>
-//import "bootstrap/dist/css/bootstrap.css";
-//import "font-awesome/css/font-awesome.css";
-import Principal from "./components/Principal";
-
 export default {
-  name: 'App',
-  components: {
-    Principal
-  }
+  name: 'App'
 }
 </script>
 
@@ -28,5 +21,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background: rgb(40,167,69);
+  background: linear-gradient(180deg, rgba(40,167,69,1) 0%, rgba(40,167,69,1) 35%, rgba(20,85,35,1) 100%);
 }
 </style>
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import Router from './routes'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -8,6 +11,8 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
 export const bus = new Vue();
 
+Vue.use(Vuex)
+Vue.use(Router)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
