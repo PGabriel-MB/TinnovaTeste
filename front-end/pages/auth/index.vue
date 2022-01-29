8e74499d578c8f374ee6c150491e38140f3932d1
PGabriel-MB <pablogrbarbosa@gmail.com>
2022-01-29 14:21:32 -0300

    crando tela de login

---
+++ front-end/pages/auth/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="
+        flex justify-center content-center h-scheen
+        bg-gradient-to-r from-green-500 to-green-900
+    ">
+        <h1>AuthPage</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AuthPage'
+}
+</script>
