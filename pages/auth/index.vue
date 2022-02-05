528739b9beb947635c52fd9575430357d67eaa14
PGabriel-MB <pablogrbarbosa@gmail.com>
2022-02-05 15:35:02 -0300

    trabalhando na tela de autenticaçao

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -1,10 +1,33 @@
 <template>
     <div class="
-        flex justify-center content-center h-full
+        flex justify-center items-center h-screen
         bg-gradient-to-r from-green-500 to-green-900
     ">
-        <form action="" class="bg-white my-10 p-10 rounded-xl">
-            <input type="text" placeholder="E-mail">
+        <form action="" class="
+            flex flex-col
+            bg-white my-10 p-12 rounded-xl
+        ">
+            <h1 class="text-center font-semibold text-green-500 mb-10 text-6xl">Carter</h1>
+            <input
+                class="
+                    border-2 border-green-500 rounded-lg p-3 mb-3
+                    focus:outline-none text-green-500 font-semibold placeholder:text-green-300
+                "
+                type="text"
+                name="email"
+                id="email"
+                placeholder="E-mail"
+            >
+            <input
+                class="
+                    border-2 border-green-500 rounded-lg p-3 mb-3
+                    focus:outline-none text-green-500 font-semibold placeholder:text-green-300
+                "
+                type="password"
+                name="password"
+                id="password"
+                placeholder="*****"
+            >
         </form>
     </div>
 </template>
